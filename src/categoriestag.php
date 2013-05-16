8ff6d70213ecd4a11e6df69c039ed0d83b6a951b
mabarroso <mabarroso@mabarroso.com>
2013-05-16 22:40:54 +0200

    Test phpunit

--- src/categoriestag.php
+++ src/categoriestag.php
@@ -1,5 +1,27 @@
-<?php
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
 
+if ( ! class_exists( 'CategoriesTag' ) ) :
 class CategoriesTag {
+    /**
+     * Setup our filters
+     *
+     * @return void
+     */
+    public function __construct() {
+        add_filter( 'the_content', array( $this, 'append_content' ) );
+    }
 
+    /**
+     * Appends "Hello WordPress Unit Tests" to the content of every post
+     *
+     * @param string $content
+     * @return string
+     */
+    public function append_content( $content ) {
+        return $content . "<p>Hello WordPress Unit Tests</p>";
+    }
 }
+
+$GLOBALS['CategoriesTag'] = new CategoriesTag();
+
+endif;
