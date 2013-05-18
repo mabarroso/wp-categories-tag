1880e253dc75b63fad628bcc82e1cf716c29254c
mabarroso <mabarroso@mabarroso.com>
2013-05-19 00:38:13 +0200

    Aceptance tests

--- tests/fixtures/taxonomy.sql
+++ tests/fixtures/taxonomy.sql
@@ -8,6 +8,10 @@ INSERT INTO /*PREFIX*/wp_terms(term_id, name, slug, term_group) VALUES ( 2, 'cat
 INSERT INTO /*PREFIX*/wp_terms(term_id, name, slug, term_group) VALUES (21, 'cat 2.1', 'cat-2-1', 0);
 INSERT INTO /*PREFIX*/wp_terms(term_id, name, slug, term_group) VALUES (22, 'cat 2.2', 'cat-2-2', 0);
 INSERT INTO /*PREFIX*/wp_terms(term_id, name, slug, term_group) VALUES (23, 'cat 2.3', 'cat-2-3', 0);
+INSERT INTO /*PREFIX*/wp_terms(term_id, name, slug, term_group) VALUES ( 3, 'cat 3',   'cat-3',   0);
+INSERT INTO /*PREFIX*/wp_terms(term_id, name, slug, term_group) VALUES (31, 'cat 3.1', 'cat-3-1', 0);
+INSERT INTO /*PREFIX*/wp_terms(term_id, name, slug, term_group) VALUES (32, 'cat 3.2', 'cat-3-2', 0);
+INSERT INTO /*PREFIX*/wp_terms(term_id, name, slug, term_group) VALUES (33, 'cat 3.3', 'cat-3-3', 0);
 INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES ( 1,  1, 'category', '', 0, 10);
 INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES (11, 11, 'category', '', 1, 11);
 INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES (12, 12, 'category', '', 1, 12);
@@ -16,3 +20,7 @@ INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, desc
 INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES (21, 21, 'category', '', 2, 21);
 INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES (22, 22, 'category', '', 2, 22);
 INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES (23, 23, 'category', '', 2, 23);
+INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES ( 3,  3, 'category', '', 0, 30);
+INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES (31, 31, 'category', '', 3, 31);
+INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES (32, 32, 'category', '', 3, 32);
+INSERT INTO /*PREFIX*/wp_term_taxonomy(term_taxonomy_id, term_id, taxonomy, description, parent, count) VALUES (33, 33, 'category', '', 3, 33);
--- tests/testsuite/categoriestagTest.php
+++ tests/testsuite/categoriestagTest.php
@@ -16,8 +16,34 @@ class CategoriesTagTest extends WP_UnitTestCase {
     $this->categoriesTag = $GLOBALS['CategoriesTag'];
   }
 
-  public function testAppendContent() {
-    $this->assertEquals('[categories id="2"]', $this->categoriesTag->append_content('[categories id="2"]'), '->append_content() appends text' );
+  public function testRoot() {
+    $response = $this->categoriesTag->append_content('[categories id="0"]');
+    $this->assertContains('cat 1</a>',  $response);
+    $this->assertContains('cat 1.1',    $response);
+    $this->assertContains('cat 1.2',    $response);
+    $this->assertContains('cat 1.3',    $response);
+    $this->assertContains('cat 2</a>',  $response);
+    $this->assertContains('cat 2.1',    $response);
+    $this->assertContains('cat 2.2',    $response);
+    $this->assertContains('cat 2.3',    $response);
+    $this->assertContains('cat 3</a>',  $response);
+    $this->assertContains('cat 3.1',    $response);
+    $this->assertContains('cat 3.2',    $response);
+    $this->assertContains('cat 3.3',    $response);
+  }
+
+  public function testChild() {
+    $response = $this->categoriesTag->append_content('[categories id="2"]');
+    $this->assertContains('cat 2.1', $response);
+    $this->assertContains('cat 2.2', $response);
+    $this->assertContains('cat 2.3', $response);
+  }
+
+  public function testShowCount() {
+    $response = $this->categoriesTag->append_content('[categories id="2" show_count="yes"]');
+    $this->assertContains('cat 2.1</a> (21)', $response);
+    $this->assertContains('cat 2.2</a> (22)', $response);
+    $this->assertContains('cat 2.3</a> (23)', $response);
   }
 
   /**
