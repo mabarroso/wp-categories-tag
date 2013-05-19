889542053f60761124bd697b4469352197f3ef01
mabarroso <mabarroso@mabarroso.com>
2013-05-19 21:34:33 +0200

    show_count faature

--- src/categoriestag.php
+++ src/categoriestag.php
@@ -27,26 +27,29 @@ class CategoriesTag {
    * @return string
    */
   public function append_content($content) {
+    $show_count  = 0;
+    $category_id = 0;
+
 		if (preg_match_all('#\[categories ([^\]]*)\]#im', $content, $tag_matches) ) {
 			$i=0;
 			foreach ($tag_matches[1] as $params) {
 				$tag = str_replace(array('[', ']'), array("\\[", "\\]"), $tag_matches[0][$i++]);
+
+				if (preg_match('#show_count="yes"#i', $params, $param_matches) ) {
+					$show_count = 1;
+				}
 				if (preg_match('#id="([^"]+)"#i', $params, $param_matches) ) {
 					$category_id = $param_matches[1];
-				} else {
-					$category_id = 0;
 				}
-				$content .= "<hr/>id = $category_id && tag = $tag";
-
 				$args = array(
 					'show_option_all'    => '',
 					'orderby'            => 'name',
 					'order'              => 'ASC',
 					'style'              => 'list',
-					'show_count'         => 0,
+					'show_count'         => $show_count,
 					'hide_empty'         => 0,
 					'use_desc_for_title' => 1,
-					'child_of'           => 0,
+					'child_of'           => $category_id,
 					'feed'               => '',
 					'feed_type'          => '',
 					'feed_image'         => '',
@@ -54,17 +57,22 @@ class CategoriesTag {
 					'exclude_tree'       => '',
 					'include'            => '',
 					'hierarchical'       => 1,
-					'title_li'           => __( 'Categories' ),
-					'show_option_none'   => __('No categories'),
+					'title_li'           => 0,
+					'show_option_none'   => 0,
 					'number'             => null,
 					'echo'               => 1,
 					'depth'              => 0,
-					'current_category'   => $category_id,
+					'current_category'   => 0,
 					'pad_counts'         => 0,
 					'taxonomy'           => 'category',
 					'walker'             => null
 				);
-				$content = preg_replace('#'.$tag.'#m', wp_list_categories($args), $content);
+				ob_start();
+				  wp_list_categories($args);
+				  $tagContent = ob_get_contents();
+        ob_end_clean();
+
+				$content = preg_replace('#'.$tag.'#m', $tagContent, $content);
 			}
 		} else {
 			$content .= '<hr/>not found';
